15396c0b519afbb8d480d7bdded55903da755e37
Ksp-Fury <74905584+Ksp-Fury@users.noreply.github.com>
2021-06-30 09:06:31 +0530

    Add files via upload

---
+++ html/js/carousal.js
@@ -0,0 +1,100 @@
+const track= document.querySelector('.carousel_track');
+const slides=Array.from(track.children);
+const nextButton=document.querySelector('.carousel_button--right');
+const prevButton=document.querySelector('.carousel_button--left');
+const dotsNav=document.querySelector('.carousel_nav');
+const dots =Array.from(dotsNav.children);
+
+const slideWidth=slides[0].getBoundingClientRect().width;
+// console.log(slideWidth);
+
+//arrange the slides next to one another in this way, so we created a fun and used loop to run
+// slides[0].style.left=slideWidth*0 +'px';
+// slides[1].style.left=slideWidth*1 + 'px';
+// slides[2].style.left=slideWidth *2 + 'px';
+const setSlidePosition=(slide,index)=>{
+    slide.style.left=slideWidth*index + 'px';
+};
+slides.forEach(setSlidePosition);
+
+
+const moveToSlide=(track,currentSlide,targetSlide)=>{
+    track.style.transform='translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+
+const updateDots=(currentDot,targetDot)=>{
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+};
+const hideShowArrows=(slides,prevbutton,nextButton,targetIndex)=>{
+    if(targetIndex===0){
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }else if(targetIndex=== slides.length-1){
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    }
+    else{
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
+//when I click left,move slides to the left
+prevButton.addEventListener('click',e=>{
+    const currentSlide=track.querySelector('.current-slide');
+    //tracking current slide
+    const prevSlide=currentSlide.previousElementSibling;
+    const currentDot= dotsNav.querySelector('.current-slide');
+    const prevDot=currentDot.previousElementSibling;
+    const prevIndex=slides.findIndex(slide=>slide===prevSlide);
+     
+    moveToSlide(track,currentSlide,prevSlide);
+    updateDots(currentDot,prevDot);
+    hideShowArrows(slides,prevButton,nextButton,prevIndex); 
+
+}) 
+
+//when I click left,move slides to the right
+nextButton.addEventListener('click',e =>{
+    const currentSlide=track.querySelector('.current-slide');
+    //tracking current slide
+    const nextSlide=currentSlide.nextElementSibling;
+    const currentDot= dotsNav.querySelector('.current-slide');
+    const nextDot=currentDot.nextElementSibling;
+    const nextIndex=slides.findIndex(slide=>slide===nextSlide);
+
+    moveToSlide(track,currentSlide,nextSlide);
+    updateDots(currentDot,nextDot);
+    hideShowArrows(slides,prevButton,nextButton,nextIndex);
+    //calling fun and moving slide
+})
+
+
+//when i clck nav indicators
+dotsNav.addEventListener('click', e=>{
+    //what indicator was clicked on?
+    const targetDot=e.target.closest('button');
+    
+    // console.log('test 1');
+    if(!targetDot) return;
+    // console.log('test 2 ');
+    
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot=dotsNav.querySelector('.current-slide');
+    const targetIndex= dots.findIndex(dot => dot=== targetDot);
+    
+    const targetSlide= slides[targetIndex];
+    
+    moveToSlide(track,currentSlide,targetSlide);
+    updateDots(currentDot,targetDot);
+
+    hideShowArrows(slides,prevButton,nextButton,targetIndex);
+
+    // console.log(dots);
+});
+// console.log(dotsNav);
+// setTimeout(corousel_track, 2000);
